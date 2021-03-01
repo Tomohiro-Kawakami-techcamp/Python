eafb12672383b7174e2dd282a331f6b7329b1cea
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-03-01 14:45:19 +0900

    変更

--- sample_class.py
+++ sample_class.py
@@ -1,2 +1,18 @@
+# coding:utf-8
 class Review:
-    pass
+    @classmethod
+    def get_review_count(cls):
+        return 0
+
+    def show_review(self):
+        print("ジャンル : " + "映画")
+        print("---------------------------")
+        print("タイトル : " + "るろうに剣心")
+        print("---------------------------")
+        print("感想 : アクションがすごい！")
+        print("---------------------------")
+
+
+review = Review()
+print(review)
+print(Review.get_review_count())
